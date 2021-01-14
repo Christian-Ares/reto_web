0ef362ab70d7d7945980999bf3bfdf33c7a0cdb9
Christian Ares <christianaresvillan@gmail.com>
2021-01-14 22:20:09 +0100

    Little changes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 const bodyParser = require('body-parser');
 const express = require("express");
-const cors = require("cors")
+const cors = require("cors");
+const path = require("path")
+
+const app = express()
+
+
+
