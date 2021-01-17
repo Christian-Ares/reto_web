aa41624c29373d4ac4c5bccc34fe29f43d6ef3ed
Christian Ares <christianaresvillan@gmail.com>
2021-01-17 14:27:10 +0100

    Routes

--- backend/routes/phones.js
+++ backend/routes/phones.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const phones = require('../../phones.json');
+
+router.get('/telefonos', (req, res, next) => {
+    res.send(phones)
+});
+module.exports = router;
